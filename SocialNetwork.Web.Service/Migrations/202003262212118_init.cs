a384a51c0219003592c58cc03c6f2e197894c3ac
FurkanHome <FurkanHome@FURKANPC>
2020-03-27 01:13:09 +0300

    Added db

---
+++ SocialNetwork.Web.Service/Migrations/202003262212118_init.cs
@@ -0,0 +1,46 @@
+﻿namespace SocialNetwork.Web.Service.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        Title = c.String(),
+                        Body = c.String(),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Password = c.String(),
+                        Name = c.String(),
+                        Lastname = c.String(),
+                        DateCreated = c.DateTime(nullable: false),
+                        DateBirth = c.DateTime(nullable: false),
+                        Email = c.String(),
+                        Role = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Posts");
+        }
+    }
+}
